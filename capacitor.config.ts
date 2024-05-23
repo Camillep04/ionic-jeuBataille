5caab8130b57446bd056321aae242e814f2e5a3b
Prothin Camille <camille.prothin@gmail.com>
2024-05-23 14:32:50 +1100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'jeu_de_bataille',
+  webDir: 'www'
+};
+
+export default config;
